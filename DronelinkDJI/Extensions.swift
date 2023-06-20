4afdddb4679ab972ccf5a8bf0278c70b9b63ef70
Bryan Taylor <bryan@dronelink.com>
2023-06-20 09:47:52 -0500

    Latest refactoring for ios localization.

--- DronelinkDJI/Extensions.swift
+++ DronelinkDJI/Extensions.swift
@@ -13,42 +13,9 @@ extension String {
     private static let LocalizationMissing = "MISSING STRING LOCALIZATION"
     
     var localized: String {
-        if let language = Dronelink.shared.locale {
-            // if system language is selected
-            if language == "" {
-                let value = DronelinkDJI.bundle.localizedString(forKey: self, value: String.LocalizationMissing, table: nil)
-                //assert(value != String.LocalizationMissing, "String localization missing: \(self)")
-                return value
-            }
-
-            // override language if possible
-            let languagePath = Bundle.main.path(forResource: language, ofType: "lproj")
-            if (languagePath != nil) {
-                let bundle = Bundle(path: languagePath!)
-                return NSLocalizedString(self, tableName: nil, bundle: bundle!, value: String.LocalizationMissing, comment: "")
-            }
-            
-            
-            // use english if override language isn't available
-            let english = "en"
-            let englishPath = Bundle.main.path(forResource: english, ofType: "lproj")
-            if (englishPath != nil) {
-                let bundle = Bundle(path: englishPath!)
-                return NSLocalizedString(self, tableName: nil, bundle: bundle!, value: String.LocalizationMissing, comment: "")
-            }
-        }
-
-        // fall back on system language if lang does not exist (perhaps because the user isn't logged in yet)
-        let value = DronelinkDJI.bundle.localizedString(forKey: self, value: String.LocalizationMissing, table: nil)
-        //assert(value != String.LocalizationMissing, "String localization missing: \(self)")
+        let value = self.localizeForLibrary(libraryBundle: DronelinkDJI.bundle, mainBundle: Bundle.main)
         return value
     }
-
-//    var localized: String {
-//        let value = DronelinkDJI.bundle.localizedString(forKey: self, value: String.LocalizationMissing, table: nil)
-//        //assert(value != String.LocalizationMissing, "String localization missing: \(self)")
-//        return value
-//    }
     
     func escapeQuotes(_ type: String = "'") -> String {
         return self.replacingOccurrences(of: type, with: "\\\(type)")
